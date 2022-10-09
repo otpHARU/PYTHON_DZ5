58ac842ad68dbbab261322146afab8ada83ac57e
Haru <harumolodec@mail.ru>
2022-10-09 20:48:37 +0700

    Zadacha4_vs_man END

---
+++ Zadacha4_vs_man.py
@@ -0,0 +1,64 @@
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. 
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# 1. Добавьте игру против бота
+# 2. Подумайте как наделить бота "интеллектом"
+
+# Человек против человека
+
+from random import randint
+
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. На столе еще осталось {value} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+value = int(input("Укажите количество конфет на столе: "))
+flag = randint(0, 2)
+if flag:
+    print(f"Первым ходит {player1}")
+else:
+    print(f"Первым ходит {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = input_dat(player2)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"{player1} урвал победу! Поздравляем!")
+else:
+    print(f"{player2} урвал победу! Поздравляем!")
+
+
+
+
+
+
+
+
+
+
+
+
+
