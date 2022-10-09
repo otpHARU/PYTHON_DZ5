168106909b11a75b0dbd062e2ce1eef1b89c21a5
Haru <harumolodec@mail.ru>
2022-10-09 19:46:21 +0700

    Zadacha1 END

--- Zadacha1.py
+++ Zadacha1.py
@@ -1,12 +1,13 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв". В тексте используется разделитель пробел.
 
-text = 'авб абв бав абв вба бав вба абв абв абв авб бав вба бав вба'
+# text = 'авб абв бав абв вба бав вба абв абв абв авб бав вба бав вба'
 
 def del_words(text):
     text = list(filter(lambda x: 'абв' not in x, text.split()))
     return " ".join(text)
 
-text = del_words(text)
-print(text)
+text = input("Введите текст: ")
+my_text = del_words(text)
+print(my_text)
 
 
